7d7f27665718e0603b0362ec9e57a0c1ea9aa80c
Emily Karen <emily.kpo2004@yahoo.com>
2025-01-10 13:19:08 -0300

    Começo de correção dos desafios 84 e 85

--- Mundo 3/aula 18/desafio 084.py
+++ Mundo 3/aula 18/desafio 084.py
@@ -3,15 +3,21 @@
 continuar = 'S'
 pessoas = list()
 inf = list()
-maior = list()
-menor = list()
+mai = men = 0
 qtde = 0
 
 print('='*15, 'Cadastro de pessoas', '='*15)
 while True:
     if continuar == 'S':
         inf.append(str(input('Nome: ')))
-        inf.append(int(input('Peso: ')))
+        inf.append(float(input('Peso: ')))
+        if len(pessoas) == 0:
+            mai = men = inf[1]
+        else:
+            if inf[1] > mai:
+                mai = inf[1]
+            if inf[1] < men:
+                men = inf[1]
         pessoas.append(inf[:])
         qtde += 1
         inf.clear()
@@ -20,12 +26,15 @@ while True:
         continuar = str(input('Letra invalida! Deseja continuar? [S/N] ')).upper()
     else:
         break
-for p in pessoas:
-    if p[1] >= 100:
-        maior.append(p[0])
-    elif p[1] <= 70:
-        menor.append(p[0])
 print('='*50)
+# existem 2 formas de contar a quantidade de pessoas que foram cadastradas. A primeira com um contador e a segunda usando o len(pessoas)
 print(f'Ao todo, você cadastrou {qtde} pessoas.')
-print(f'Pessoas com o peso maior ou igual a 100 Kg: {maior}.')
-print(f'Pessoas com o peso menor ou igual a 70 Kg: {menor}.')
+print(f'Pessoas com o peso maior ou igual a {mai} Kg: ', end='')
+for p in pessoas:
+    if p[1] == mai:
+        print(p[0], end=' ')
+print()
+print(f'Pessoas com o peso menor ou igual a {men} Kg: ', end='')
+for p in pessoas:
+    if p[1] == men:
+        print(p[0], end='')
